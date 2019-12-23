773fd1c32432e3fe5fe7470403082c675641dce1
shruthi-devaraj-webjet <57157976+shruthi-devaraj-webjet@users.noreply.github.com>
2019-12-23 12:14:21 +1100

    updating TravellerProfile

--- MyBooking/TravellerProfile.js
+++ MyBooking/TravellerProfile.js
@@ -6,20 +6,30 @@ chai.use(chaiHttp);
 describe('TravelerProfile',()=>{
 
     it('Get Traveller Profile List',()=>{
-        console.log("inside method")
-        return chai.request('https://services.dev.webjet.com.au')
+               return chai.request('https://services.dev.webjet.com.au')
         .get('/api/customer/travellerprofile/list?')
         .set('x-customer-email','shruthi.devaraj@webjet.com.au')
         .set('x-customer-reference-id','46076e37-99a8-4562-ba2f-32a6082edd2a')
         .then(function(response){
-            console.log("inside method 2")
 
-        for(let count=1; count<(response.body).length; count++){
+            var travellerProfile = response.body.filter(tp => tp.travellerId == '74c817c8-12ea-4e83-933c-28cf005929ac')
+            var travellerProfileLength =travellerProfile.length;
+            console.log(travellerProfile);
 
-            expect(response.body[count].travellerId).equal("13770a29-88bd-43be-a20f-8308405f0827")
-        
-       }  
-        
+    if(!(travellerProfile.length == 0)){
+         console.log('inside if',travellerProfile.Length)      
+        for(var index =0; index<travellerProfile.length;index++){
+             expect(travellerProfile[index]).to.have.property("travellerId").equals('74c817c8-12ea-4e83-933c-28cf005929ac');
+             expect(travellerProfile[index]).to.have.property('travellerEmail').equals('shruthi.devaraj@webjet.com.au'); 
+             expect(travellerProfile[index]).to.have.property('mobilePhone').to.be.null;  
+             expect(travellerProfile[index]).to.have.property('frequentFlyerPrograms').to.be.empty;  
+            
+            }
+               }       else{
+                  
+                   expect(false,'Could not find the traveller profile ').to.not.be.false;
+               }   
+ 
         })
 
     })
