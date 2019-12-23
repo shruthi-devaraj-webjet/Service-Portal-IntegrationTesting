43eaecbb23a4ad48c08d34bd8486e93fb9b1cc1d
shruthi-devaraj-webjet <57157976+shruthi-devaraj-webjet@users.noreply.github.com>
2019-12-23 12:11:35 +1100

    Create MyBookingDetails.js

---
+++ MyBooking/MyBookingDetails.js
@@ -0,0 +1,23 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('My booking', function(){
+
+    it('My booking details', function(){
+      return chai.request('https://services.dev.webjet.com.au/') 
+      .get('api/customer/mybookingdetails/get?itineraryId= 218305')
+      .set('x-customer-email','shruthidevarajwebjettest@gmail.com')
+      .set('x-customer-reference-id','d7159a9e-38e2-4172-8d9d-7c4337f67ac8')
+      .then(function(response){
+          console.log(response.body.travellers.length)
+          expect(response.body).to.have.property('bookingStatus').equals('Confirmed');
+          
+          for(var  i=0; i<response.body.travellers.length;i++){
+              expect(response.body.travellers[i]).to.have.property('firstName').equals('shruthi')
+          }
+        })
+    })
+})
