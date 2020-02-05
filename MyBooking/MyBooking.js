2b262c50cf56b8a902aac92df0096af520ebc836
shruthi devaraj <shruthi.devaraj@webjet.com.au>
2020-02-05 13:29:16 +1100

    commiting code for new booking

--- MyBooking/MyBooking.js
+++ MyBooking/MyBooking.js
@@ -3,7 +3,6 @@ let chaiHttp = require('chai-http');
 let expect = chai.expect;
 
 chai.use(chaiHttp);
-//Commenting 
 
 describe('My Booking Health Test', function(){
 
@@ -26,23 +25,24 @@ describe('My Booking list test',function(){
     it('returns number of booking', () =>{
         return chai.request('https://services.dev.webjet.com.au')
         .get('/api/customer/mybookings/v2/list?sortBy=TravelDate&filterBy=Upcoming&travellerName=&webjetReference=&productType=&dateFrom=&dateTo')
-        .set('x-customer-reference-id', '46076e37-99a8-4562-ba2f-32a6082edd2a')
-        .set('x-customer-email','shruthi.devaraj@webjet.com.au')
+        .set('x-customer-reference-id', '492b00ba-554f-4d66-a023-3605d52ee09b')
+        .set('x-customer-email','shruthimdevaraj@gmail.com')
         .then(function(res){
 //             const booking = res.body.bookings.filter(x => x.itineraryId == 213576);
 //             console.log(booking);
 //             expect(booking != null);
 //             expect(booking.itineraryId).to.have.property('bookingStatus').equal('Confirmed')
 //         looping over the response data
-         for(var i =0; i<(res.body.bookings).length; i++){
-           
 
-            expect(res).to.have.status(200);
-              if (res.body.bookings[i].itineraryId === 213576) {
+expect(res.body.bookings[0].itineraryId).equal(223233)
+        for(var i =0; i<(res.body.bookings).length; i++){
+          
+               expect(res).to.have.status(200);
+                if (res.body.bookings[i].itineraryId === 223233) {
                 expect(res.body.bookings[i]).to.have.property('bookingStatus').equal('Confirmed');
                 expect(res.body.bookings[i].travellers[i]).to.have.property('title').equal('Miss');
-                expect(res.body.bookings[i].travellers[i]).to.have.property('firstName').equal('After');
-                expect(res.body.bookings[i].travellers[i]).to.have.property('lastName').equal('Booking');
+                expect(res.body.bookings[i].travellers[i]).to.have.property('firstName').equal('shruthi');
+                expect(res.body.bookings[i].travellers[i]).to.have.property('lastName').equal('Devaraj');
                 expect(res.body.bookings[i].flightSegments[i]).to.have.property('departureCityName').equal('Melbourne');
                 expect(res.body.bookings[i].flightSegments[i]).to.have.property('departureCountryName').equal('Australia');
                 expect(res.body.bookings[i].flightSegments[i]).to.have.property('departureCountryCode').equal('AU');
@@ -54,5 +54,3 @@ describe('My Booking list test',function(){
 
     })
 })
-
-
